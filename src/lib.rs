8a5f362c730518687ab89a547b40ed3e6b57d8e1
jakeg00dwin <jakegoodwin@gorge.works>
2024-01-31 20:09:08 -0800

    added a bunch of traits for that sensor shims must impliment.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 #![cfg_attr(not(test), no_std)]
 
 
+
+#[allow(dead_code)]
+struct SensorData {
+    data: u8,
+}
+
 #[allow(dead_code)]
 struct SensorInterface {
     sensor_name: &'static str,
@@ -8,6 +14,21 @@ struct SensorInterface {
     data_names: &'static str,
 }
 
+pub trait InitSensor {
+    fn init_sensor(&self) -> Result<T, E>;
+}
+
+pub trait ReadSensor {
+    fn read_sensor(&self) -> SensorData;
+}
+
+pub trait GetFormat {
+    fn get_format(&self) -> &'static str;
+}
+
+pub trait GetNames {
+    fn get_names(&self) -> &'static str;
+}
 
 #[cfg(test)]
 mod sensor_interface_tests {
