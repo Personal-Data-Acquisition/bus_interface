d57f0d62f6809018c2ee044cc63c6258edf9bacb
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-02 10:29:51 -0800

    updated code a bit to put all the trait methods into a single trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,44 @@
 #![cfg_attr(not(test), no_std)]
 
+//This gives the methods that must be implimented for any sensor that
+//impliments the SensorInterface trait.
+pub trait SensorInterface {
+    fn new() -> SensorInterface;
 
+    fn init_sensor(&self) -> Result<T, E>;
+
+    fn read_sensor(&self) -> SensorData;
+
+    fn get_format(&self) -> &'static str;
+
+    fn get_names(&self) -> &'static str;
+}
 
 #[allow(dead_code)]
 struct SensorData {
     data: u8,
 }
 
+//This is a structure just used to show how it works,
+//you can think of this as a fake sensor; or an example of what you
+//will need to create for the sensor interface.
 #[allow(dead_code)]
-struct SensorInterface {
+struct ExampleSensor{
     sensor_name: &'static str,
     data_types: &'static str,
     data_names: &'static str,
 }
 
-pub trait InitSensor {
-    fn init_sensor(&self) -> Result<T, E>;
+/*
+ * This section shows how you should impliment
+ * the traits for an sensor kinda.
+ * 
+ */
+impl SensorInterface for ExampleSensor {
+
 }
 
-pub trait ReadSensor {
-    fn read_sensor(&self) -> SensorData;
-}
 
-pub trait GetFormat {
-    fn get_format(&self) -> &'static str;
-}
-
-pub trait GetNames {
-    fn get_names(&self) -> &'static str;
-}
 
 #[cfg(test)]
 mod sensor_interface_tests {
@@ -40,24 +50,7 @@ mod sensor_interface_tests {
     }
 
     #[test]
-    fn init_sensor() {
-        assert!(false);
-    }
-
-    #[test]
-    fn read_sensor() {
-        assert!(false);
-    }
-
-    #[test]
-    fn get_format() {
-        assert!(false);
-    }
-
-    #[test]
-    fn get_names() {
-        assert!(false);
+    fn check_traits() {
+        let exam = ExampleSensor::new();
     }
 }
-
-
