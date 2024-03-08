a42477f508a457a09f8effc81ec5dc355544b6ba
jakeg00dwin <jakegoodwin@gorge.works>
2024-03-07 19:15:15 -0800

    extracted out the return value type into it's own module

---
+++ src/cmd_return.rs
@@ -0,0 +1,22 @@
+// The data that gets returned from the command requests.
+#![allow(dead_code)]
+#[derive(Debug)]
+pub struct CmdReturn {
+    name: String,
+    format: String,
+    data_names: String,
+    raw_bytes: Vec<u8>,
+}
+
+impl CmdReturn {
+    pub fn new() -> CmdReturn {
+        let ret = CmdReturn{
+            name: String::new(),
+            format: String::new(),
+            data_names: String::new(),
+            raw_bytes: vec![],
+        };
+        ret
+    }
+
+}
