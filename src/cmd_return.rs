386d2514df8678a3da925ae96ac168c08584e49f
jakeg00dwin <jakegoodwin@gorge.works>
2024-03-07 20:53:10 -0800

    added passing implementation of method to convert data to json

--- src/cmd_return.rs
+++ src/cmd_return.rs
@@ -3,7 +3,7 @@
 #[derive(Debug)]
 pub struct CmdReturn {
     name: String,
-    format: String,
+    format: Vec<String>,
     data_names: Vec<String>,
     raw_bytes: Vec<u8>,
 }
@@ -12,7 +12,7 @@ impl CmdReturn {
     pub fn new() -> CmdReturn {
         let ret = CmdReturn{
             name: String::new(),
-            format: String::new(),
+            format: vec![],
             data_names: vec![], 
             raw_bytes: vec![],
         };
@@ -24,17 +24,96 @@ impl CmdReturn {
         //1. add the name json.
         json.push_str("{\"name\":\"");
         json.push_str(&self.name);
-        json.push_str("\", ");
+        json.push_str("\"");
+
+        let data_strings = self.bytes_to_strings();
 
         //2. Add the data.
-        //. figure out how many vars of data we have.
+        for i in 0..self.data_names.len() {
+            json.push_str(", \"");
+            json.push_str(&self.data_names[i]);
+            json.push_str("\":\"");
+            json.push_str(&data_strings[i]);
+            json.push_str("\"");
+        }
+        
+        json.push_str("}");
+
         json
     }
 
-    fn num_vars(&self) -> usize {
-        return 0    
+    fn bytes_to_strings(&self) -> Vec<String> {
+        let mut data: Vec<String> = vec![]; 
+        let mut byte_index: usize = 0;
+        
+        for fmt in &self.format {
+            if fmt.contains("u8") {
+                data.push(format!("{}",self.raw_bytes[byte_index]));    
+                byte_index += 1;
+            }
+            else if fmt.contains("u16") {
+                let tmp: u16 = bytes_to_u16(&self.raw_bytes, byte_index);
+                data.push(format!("{}",tmp));    
+                byte_index += 2;
+            }
+
+            else if fmt.contains("i16") {
+                let tmp: i16 = bytes_to_i16(&self.raw_bytes, byte_index);
+                data.push(format!("{}",tmp));    
+                byte_index += 2;
+            }
+
+            else if fmt.contains("u32") {
+                let tmp: u32 = bytes_to_u32(&self.raw_bytes, byte_index);
+                data.push(format!("{}",tmp)); 
+                byte_index += 4;
+            }
+
+            else if fmt.contains("i32") {
+                let tmp: i32 = bytes_to_i32(&self.raw_bytes, byte_index);
+                data.push(format!("{}",tmp)); 
+                byte_index += 4;
+            }
+        }
+
+        data
     }
 
+}
+
+fn bytes_to_u16(b: &Vec<u8>, start: usize) -> u16 {
+        let tmp: u16 =
+            ((b[start] as u16 )<< 8) | 
+            (b[start + 1] as u16);
+        tmp
+}
+
+
+fn bytes_to_i16(b: &Vec<u8>, start: usize) -> i16 {
+        let tmp: i16 =
+            ((b[start] as i16 )<< 8) | 
+            (b[start + 1] as i16);
+        tmp
+}
+
+
+fn bytes_to_u32(b: &Vec<u8>, start: usize) -> u32 {
+    let tmp: u32 = 
+        ((b[start] as u32 )<< 24) | 
+        ((b[start + 1] as u32 )<< 16) | 
+        ((b[start + 2] as u32 )<< 8) | 
+        (b[start + 3] as u32);
+        tmp
+}
+
+
+fn bytes_to_i32(b: &Vec<u8>, start: usize) -> i32 {
+    let tmp: i32 = 
+        ((b[start] as i32 )<< 24) | 
+        ((b[start + 1] as i32 )<< 16) | 
+        ((b[start + 2] as i32 )<< 8) | 
+        (b[start + 3] as i32);
+        tmp
 }
 
 //Tests for the structure
@@ -45,7 +124,8 @@ mod test_cmdreturn {
     fn setup() -> CmdReturn {
         let mut new_response = CmdReturn::new();
         new_response.name = String::from("aht20");
-        new_response.format = String::from("u16 u16");
+        new_response.format.push(String::from("u16"));
+        new_response.format.push(String::from("u16"));
         new_response.data_names.push(String::from("Temp"));
         new_response.data_names.push(String::from("Humid"));
         new_response.raw_bytes = vec!(0, 255, 0, 255);
