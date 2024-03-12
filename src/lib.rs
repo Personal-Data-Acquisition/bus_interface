09be1a8805087f7a26c768050901a509104c6328
jakeg00dwin <jakegoodwin@gorge.works>
2024-03-11 18:36:33 -0700

    wrote new test for data_names_request()

--- src/lib.rs
+++ src/lib.rs
@@ -423,4 +423,22 @@ mod sensor_interface_tests {
         //check the formatting sent back.
         assert_eq!(READING_TYPES.as_bytes() , td.bus.spy_data());
     }
+
+    #[test]
+    fn data_names_request() {
+
+        let mut td = setup();
+        
+        let cmd_result = send_bus_command(&mut td.bus, &ControllerCommand::DnamesRequest);
+        assert!(cmd_result.is_ok());
+
+        assert!(td.bus.spy_id() == 0);
+        assert!(td.bus.spy_data()[0] == ControllerCommand::DnamesRequest as u8);
+
+        let handler_result = handle_bus_command(0x001, &mut td.bus, &mut td.sens);
+        assert!(handler_result.is_ok());
+
+        //check the formatting sent back.
+        assert_eq!(READING_NAMES.as_bytes() , td.bus.spy_data());
+    }
 }
