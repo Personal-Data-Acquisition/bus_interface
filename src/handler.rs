3f0c896a42ae7257d6c19cc356663004ee68bfa0
jakeg00dwin <jakegoodwin@gorge.works>
2024-03-15 12:29:27 -0700

    removed dead code

--- src/handler.rs
+++ src/handler.rs
@@ -5,7 +5,8 @@
  * Desc: File to be included for embedded devices. 
  */
 
-//include!("fake_sensor.rs");
+
+#![no_std]
 
 pub fn handle_bus_command(slv_id: u32, bus: &mut dyn Bus, sens: &mut dyn SensorInterface) -> Result<(), BusError>{
     
