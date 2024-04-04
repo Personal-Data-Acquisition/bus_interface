8f1c55b28f68a681ce582f0142bb6754a4e401f0
jakeg00dwin <jakegoodwin@gorge.works>
2024-04-03 18:29:21 -0700

    added white space

--- src/handler.rs
+++ src/handler.rs
@@ -5,6 +5,7 @@
  * Desc: File to be included for embedded devices. 
  */
 
+
 pub fn handle_bus_command(slv_id: u32, bus: &mut dyn Bus, sens: &mut dyn SensorInterface) -> Result<(), BusError>{
     
     //get the cmd out of the message.
